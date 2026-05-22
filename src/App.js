6948a2cf8167f7a5cbe693914e2e201e2a91a908
zeeshankashif <zeeshankashif.100m@gmail.com>
2026-05-22 20:38:06 +0500

    BMW

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const PROJECTS = [
     blurb: 'Cool and Stylish',
     href: 'https://sneaky-style.vercel.app/',
     image:
-      'sneaky.png',
+      'aky.png',
     },
   {
       name: 'Pixify AI',
@@ -66,7 +66,7 @@ const PROJECTS = [
       blurb: 'Powered by ZEXAN',
       href: 'https://pixify-ai-tau.vercel.app/',
       image:
-      'pixify.png',
+      'https://plus.unsplash.com/premium_photo-1675876811760-de275fde11b9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHB8ZW58MHx8MHx8fDA%3D',
     },
   ];
 
