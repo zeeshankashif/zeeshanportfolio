4e503f86bc78aa5b9c0795bb00d74092df4a4204
zeeshankashif <zeeshankashif.100m@gmail.com>
2026-05-22 21:48:02 +0500

    PORSCHE

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const PROJECTS = [
     blurb: 'Cool and Stylish',
     href: 'https://sneaky-style.vercel.app/',
     image:
-      'aky.png',
+      'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHx8MHx8fDA%3D',
     },
   {
       name: 'Pixify AI',
